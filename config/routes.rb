76d27235acdd1373f50cd5b2ddbeba138adb6eec
Tim Paisley <tim.paisley@wcc.govt.nz>
2019-01-09 11:29:04 +1300

    Only show sign up button if registerable

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :visits
   resources :locations
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  devise_scope :user do
+    root to: "home#index"
+  end
+
   get '*path', to: "guide#index"
 end
